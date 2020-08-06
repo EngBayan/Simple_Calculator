9b7fe77af8ee2f785e33f15396e964412d7b8691
EngBayan <66920488+EngBayan@users.noreply.github.com>
2020-08-06 14:25:41 +0300

    Add files via upload

---
+++ calculator_using_python.py
@@ -0,0 +1,38 @@
+print("Addition Operation")
+num1= input("enter a number ,please   :")
+num2=input("enter another number,please   :")
+the_resault= int(num1)+int(num2)
+print("dear user ,the summation of the two numbers you've entered is \n ")
+print(the_resault)
+
+print("Multiplication Operation")
+num1=input("enter the first number,please")
+num2=input("enter the second number,please")
+the_resault= int(num1)* int(num2)
+print("dear user ,the resault of multiplying the two numbers you've entered is \n ")
+print(the_resault)
+
+print("Modulo Operation")
+num1= input("enter a number ,please   :")
+num2=input("enter another number,please   :")
+the_resault= float(num1)%float(num2)
+print(the_resault)
+
+print("Division Operation")
+num1=input("enter the first number,please")
+num2=input("enter the second number,please")
+the_resault= int(num1)/int(num2)
+print(the_resault)
+
+print("Exponential Operation")
+base = int(input("dear user,enter the base"))
+exponent =int(input("dear user,now enter the exponent"))
+resault= pow(int(base),int(exponent))
+print("dear user the resault of the exponential operation is   :")
+print(resault)
+
+
+
+
+
+ 
